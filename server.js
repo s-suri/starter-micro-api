c65d57fd3c4f8126fc32bd2ac0b77a35ec687f0c
Surender Kumar <56398576+s-suri@users.noreply.github.com>
2022-11-24 04:14:26 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import paytmInti from "./paytm-init";
+import paytmVrfy from "./paytm-vrfy";
+import prodPaytmInti from "./prod-paytm-init";
+import prodPaytmVrfy from "./prod-paytm-vrfy";
+
+
+const app = express();
+const PORT = process.env.PORT || 4416;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.get("/", (rq, rs) => rs.json({ message: "good attempt bhai.." }));
+app.post("/payment-initiate", paytmInti);
+app.post("/payment-verify", paytmVrfy);
+app.post("/king-rathode-payment-initiate", prodPaytmInti);
+app.post("/king-rathode-payment-verify", prodPaytmVrfy);
+
+app.use((err, req, res, next) => console.log(err));
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
